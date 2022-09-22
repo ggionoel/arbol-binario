0e26de81e28b413b442ba902a0365b41b4126270
GiovanyNoel <gionoeluruchi007@gmail.com>
2022-09-22 19:44:18 -0400

    Adicion de menu - practica 1

--- src/arbolbinario/ArbolBinario.java
+++ src/arbolbinario/ArbolBinario.java
@@ -5,6 +5,8 @@
  */
 package arbolbinario;
 
+import java.util.Scanner;
+
 /**
  *
  * @author omarl
@@ -15,37 +17,71 @@ public class ArbolBinario {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ArbolEnteros arbol = new ArbolEnteros();
-        arbol.insertar(1);
-        arbol.insertar(2);
-        arbol.insertar(3);
-        arbol.insertar(4);
-        arbol.insertar(0);
-        System.out.println("Recorriendo inorden:");
-        arbol.inorden();
-        System.out.println("Recorriendo postorden:");
-        arbol.postorden();
-        System.out.println("Recorriendo preorden:");
-        arbol.preorden();
-        System.out.println(arbol.existe(1)); // true
-        System.out.println(arbol.existe(7)); // false
-        System.out.println(arbol.existe(0)); // true
+          int opcionMenu = 0;
+        Scanner leer = new Scanner(System.in);
+        Scanner leer2 = new Scanner(System.in);
+        Scanner leer3 = new Scanner (System.in);
 
-        ArbolCadenas arbolCadenas = new ArbolCadenas();
-        arbolCadenas.insertar("Luis");
-        arbolCadenas.insertar("Chris");
-        arbolCadenas.insertar("Zelda");
-        arbolCadenas.insertar("Cuphead");
-        arbolCadenas.insertar("Leon");
-        System.out.println("Recorriendo inorden:");
-        arbolCadenas.inorden();
-        System.out.println("Recorriendo postorden:");
-        arbolCadenas.postorden();
-        System.out.println("Recorriendo preorden:");
-        arbolCadenas.preorden();
-        System.out.println(arbolCadenas.existe("Luis")); // true
-        System.out.println(arbolCadenas.existe("Claire")); // false
-        System.out.println(arbolCadenas.existe("Zelda")); // true
+        ArbolEnteros arbEnt = new ArbolEnteros();
+        ArbolCadenas arbCad = new ArbolCadenas();
+        do {
+            System.out.println("\n\n");
+            System.out.println(" |                 MENÚ                         |");
+            System.out.println(" |----------------------|-----------------------|");
+            System.out.println(" | 1. Insertar datos    | 8. Recorre InOrden L  |");
+            System.out.println(" | 2. Buscar dato       | 9. Recorre PostOerde L|");
+            System.out.println(" | 3. Recorre InOrden   | 10. Recorre PreOrden L|");
+            System.out.println(" | 4. Recorre PostOrden | 11. _______________   |");
+            System.out.println(" | 5. Recorre PreOrden  | 12. _______________   |");
+            System.out.println(" | 6. Insestar dato L   | 13. _______________   |");
+            System.out.println(" | 7. Buscar dato L     | 20. Salir             |");
+            opcionMenu = leer.nextInt();
+            switch (opcionMenu) {
+                case 1:
+                    System.out.println("Insertar datos: ");
+                    int dato = leer2.nextInt();
+                    arbEnt.insertar(dato);
+                    break;
+                case 2:
+                    System.out.println("Buscar dato: ");
+                    int buscar = leer2.nextInt();
+                    arbEnt.existe(buscar);
+                    System.out.println("Dato Encontrado: " + buscar);
+                    break;
+                case 3:
+                    System.out.println("Recorriendo inOrden");
+                    arbEnt.inorden();
+                    break;
+                case 4:
+                    System.out.println("Recorriendo postOrden");
+                    arbEnt.postorden();
+                    break;
+                case 5:
+                    System.out.println("Recorriendo preOrden");
+                    arbEnt.preorden();
+                    break;
+                case 6:
+                    System.out.println("Insertar datos Letras");
+                    String valor = leer3.nextLine();
+                    arbCad.insertar(valor);
+                    break;
+                case 7:
+                    System.out.println("Buscar dato Letras");
+                    String buscar1 = leer3.nextLine();
+                    System.out.println("Dato Encontrado: "+buscar1);
+                    break;
+                case 8:
+                    System.out.println("Recorriendo inOrden Letras");
+                    arbCad.inorden();
+                case 9:
+                    System.out.println("Recorriendo postOrden Letras");
+                    arbCad.postorden();
+                case 10:
+                    System.out.println("Recorriendo preOrden Letras");
+                    arbCad.preorden();
+            }
+        } while (opcionMenu != 20);
+
     }
 
 }
